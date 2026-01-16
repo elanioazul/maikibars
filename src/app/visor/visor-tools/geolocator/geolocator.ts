14e91ab0991d834d2d8deb3d25c64fe59c93786f
hugo <hugo.herrador@gmail.com>
2026-01-16 14:46:04 +0100

    implement geolocator

--- src/app/visor/visor-tools/geolocator/geolocator.ts
+++ src/app/visor/visor-tools/geolocator/geolocator.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Tool } from '../warehouse/tool/tool';
+import { MapService } from 'src/app/core/services/map.service';
+import { Marker } from 'maplibre-gl';
 
 @Component({
   selector: 'app-geolocator',
@@ -8,5 +10,43 @@ import { Tool } from '../warehouse/tool/tool';
   styleUrl: './geolocator.scss',
 })
 export class Geolocator extends Tool {
+  mapService = inject(MapService);
 
+  isLocating = signal(false);
+
+  override onClick(): void {
+    if (!navigator.geolocation) {
+          console.error('Geolocation is not supported by your browser');
+          return;
+    }
+
+    this.isLocating.set(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { longitude, latitude } = position.coords;
+
+        this.mapService.map?.flyTo({
+          center: [longitude, latitude],
+          zoom: 14,
+          essential: true
+        });
+
+        new Marker({ color: '#3f51b5' })
+          .setLngLat([longitude, latitude])
+          .addTo(this.mapService.map!);
+
+        this.isLocating.set(false);
+      },
+      (error) => {
+        console.error('Error obtaining location', error);
+        this.isLocating.set(false);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+    );
+  }
 }
