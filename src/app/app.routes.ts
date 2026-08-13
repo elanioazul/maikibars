2dfc0077572285e2f036453c2f75c59420711b20
hugo <hugo.herrador@gmail.com>
2026-08-13 12:26:37 +0200

    add login comp and login auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { Visor } from '@visor/visor';
 export const routes: Routes = [
-    {
-        path: 'visor',
-        loadComponent: () => import('./visor/visor').then(m => m.Visor),
-    },
-    {
-      path: '',
-      redirectTo: 'visor',
-      pathMatch: 'full',
-    }
-  ];
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, title: 'Sign in' },
+  {
+    path: 'visor',
+    component: Visor,
+    title: 'Visor',
+    canActivate: [authGuard],
+  },
+  { path: '**', redirectTo: '/login' },
+];
